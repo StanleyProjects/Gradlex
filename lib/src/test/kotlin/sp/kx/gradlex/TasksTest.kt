e0869352be1ebaaf46553251efbc4ae9375c485a
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-21 23:16:53 +0300

    tasks test

--- lib/src/test/kotlin/sp/kx/gradlex/TasksTest.kt
+++ lib/src/test/kotlin/sp/kx/gradlex/TasksTest.kt
@@ -4,6 +4,7 @@ import org.gradle.api.DefaultTask
 import org.gradle.internal.FileUtils
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import java.nio.file.Files
 import java.util.concurrent.atomic.AtomicInteger
@@ -22,6 +23,23 @@ internal class TasksTest {
         val task = project.tasks.get<DefaultTask>("foo", "bar", "", "baz", " ", "qux")
         assertEquals(expected, task.name)
         assertEquals(1, counter.get())
+    }
+
+    @Test
+    fun getErrorTest() {
+        val projectDir = Files.createTempDirectory(this::class.java.name).toFile().let(FileUtils::canonicalize)
+        val project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+        val counter = AtomicInteger(0)
+        assertEquals(0, counter.get())
+        val expected = "fooBarBazQux"
+        open class FooTask : DefaultTask()
+        project.tasks.create(expected, FooTask::class.java) {
+            counter.incrementAndGet()
+        }
+        open class BarTask : DefaultTask()
+        assertThrows(IllegalStateException::class.java) {
+            project.tasks.get<BarTask>("foo", "bar", "", "baz", " ", "qux")
+        }
     }
 
     @Test

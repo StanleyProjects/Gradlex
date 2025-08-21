9b9347ee3ff5167ed5754be148cefa68fbf468e6
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-21 22:04:44 +0300

    github test

--- lib/src/test/kotlin/sp/kx/gradlex/GitHubTest.kt
+++ lib/src/test/kotlin/sp/kx/gradlex/GitHubTest.kt
@@ -1,18 +1,60 @@
 package sp.kx.gradlex
 
+import org.gradle.internal.FileUtils
+import org.gradle.testfixtures.ProjectBuilder
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import java.net.URI
+import java.nio.file.Files
 
 internal class GitHubTest {
+    @Test
+    fun uriTest() {
+        val owner = "foo"
+        val name = "foo"
+        val gh = GitHub.Repository(owner = owner, name = name)
+        val expected = URI("https://github.com/$owner/$name")
+        val actual = gh.uri()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun pagesTest() {
+        val owner = "foo"
+        val name = "foo"
+        val gh = GitHub.Repository(owner = owner, name = name)
+        val expected = URI("https://$owner.github.io/$name")
+        val actual = gh.pages()
+        assertEquals(expected, actual)
+    }
+
     @Test
     fun releaseTest() {
         val owner = "foo"
         val name = "foo"
-        val version = "baz"
         val gh = GitHub.Repository(owner = owner, name = name)
+        val version = "baz"
         val expected = URI("https://github.com/$owner/$name/releases/tag/$version")
         val actual = gh.release(version = version)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun assembleTest() {
+        val owner = "foo"
+        val name = "foo"
+        val gh = GitHub.Repository(owner = owner, name = name)
+        val version = "baz"
+        val projectDir = Files.createTempDirectory(this::class.java.name).toFile().let(FileUtils::canonicalize)
+        val project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+        val target = project.layout.projectDirectory.file("foo")
+        val expected = """
+            repository:
+             owner: '$owner'
+             name: '$name'
+            version: '$version'
+        """.trimIndent()
+        val actual = gh.assemble(version = version, target = target).readText()
         assertEquals(expected, actual)
     }
 }

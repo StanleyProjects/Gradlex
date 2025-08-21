f18b5ebf5342fff53b63b176ef59e92fd12d6463
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-21 22:19:44 +0300

    dirs test

--- lib/src/test/kotlin/sp/kx/gradlex/DirsTest.kt
+++ lib/src/test/kotlin/sp/kx/gradlex/DirsTest.kt
@@ -3,6 +3,7 @@ package sp.kx.gradlex
 import org.gradle.internal.FileUtils
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 import java.nio.file.Files
 
@@ -27,5 +28,45 @@ internal class DirsTest {
         projectDir.resolve("foo/bar").writeText(expected)
         val dir = project.layout.projectDirectory.dir("foo")
         assertEquals(expected, dir.eff("bar").readText())
+    }
+
+    @Test
+    fun effNotExistsTest() {
+        val projectDir = Files.createTempDirectory(this::class.java.name).toFile().let(FileUtils::canonicalize)
+        val project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+        val name = "foo"
+        val issuer = projectDir.resolve(name)
+        check(!issuer.exists())
+        assertThrows(IllegalStateException::class.java) {
+            project.layout.projectDirectory.eff(name)
+        }
+    }
+
+    @Test
+    fun effNotFileTest() {
+        val projectDir = Files.createTempDirectory(this::class.java.name).toFile().let(FileUtils::canonicalize)
+        val project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+        val name = "foo"
+        val issuer = projectDir.resolve(name)
+        check(issuer.mkdirs())
+        check(issuer.exists())
+        check(!issuer.isFile)
+        assertThrows(IllegalStateException::class.java) {
+            project.layout.projectDirectory.eff(name)
+        }
+    }
+
+    @Test
+    fun effEmptyTest() {
+        val projectDir = Files.createTempDirectory(this::class.java.name).toFile().let(FileUtils::canonicalize)
+        val project = ProjectBuilder.builder().withProjectDir(projectDir).build()
+        val name = "foo"
+        val issuer = projectDir.resolve(name)
+        issuer.writeText("")
+        check(issuer.exists())
+        check(issuer.isFile)
+        assertThrows(IllegalStateException::class.java) {
+            project.layout.projectDirectory.eff(name)
+        }
     }
 }

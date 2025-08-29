13ec61ff58b07a950d1f3f48a52ff814a64d604f
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-29 13:48:43 +0300

    fix docs

--- lib/src/main/kotlin/sp/kx/gradlex/Projects.kt
+++ lib/src/main/kotlin/sp/kx/gradlex/Projects.kt
@@ -4,6 +4,21 @@ import org.gradle.api.Project
 import org.gradle.api.file.Directory
 import org.gradle.api.file.ProjectLayout
 
+/**
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val dir = project.buildDir()
+ * val file = dir.asFile
+ * if (file.exists()) {
+ *     assertTrue(file.isDirectory)
+ * }
+ * ```
+ * @return the build [Directory] for the project.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.0
+ * @see [ProjectLayout.getBuildDirectory]
+ */
 fun Project.buildDir(): Directory {
     return layout.buildDirectory.get()
 }
@@ -30,10 +45,40 @@ val Project.buildSrc: ProjectLayout get() {
     )
 }
 
+/**
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val dir = project.layout.buildDir()
+ * val file = dir.asFile
+ * if (file.exists()) {
+ *     assertTrue(file.isDirectory)
+ * }
+ * ```
+ * @return the build [Directory] for the project.
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.0
+ * @see [ProjectLayout.getBuildDirectory]
+ */
 fun ProjectLayout.buildDir(): Directory {
     return buildDirectory.get()
 }
 
+/**
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val dir = project.layout.dir("foo")
+ * val file = dir.asFile
+ * if (file.exists()) {
+ *     assertTrue(file.isDirectory)
+ * }
+ * ```
+ * @return a [Directory] whose location is the given [path], resolved relative to [ProjectLayout.getProjectDirectory].
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.0
+ * @see [ProjectLayout.getProjectDirectory]
+ */
 fun ProjectLayout.dir(path: String): Directory {
     return projectDirectory.dir(path)
 }

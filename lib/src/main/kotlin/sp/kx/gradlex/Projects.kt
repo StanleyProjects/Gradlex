fc3d23d9e8c369e7e95c006763c40fe3ef4ddc6d
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-08-29 13:34:35 +0300

    fix docs

--- lib/src/main/kotlin/sp/kx/gradlex/Projects.kt
+++ lib/src/main/kotlin/sp/kx/gradlex/Projects.kt
@@ -8,6 +8,21 @@ fun Project.buildDir(): Directory {
     return layout.buildDirectory.get()
 }
 
+/**
+ * Provides access to various important directories for "buildSrc" directory of [Project].
+ *
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val file = project.buildSrc.projectDirectory.asFile
+ * if (file.exists()) {
+ *     assertTrue(file.isDirectory)
+ *     assertEquals("buildSrc", file.name)
+ * }
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.0
+ */
 val Project.buildSrc: ProjectLayout get() {
     return FinalProjectLayout(
         objectFactory = rootProject.objects,
--- lib/src/main/kotlin/sp/kx/gradlex/Tasks.kt
+++ lib/src/main/kotlin/sp/kx/gradlex/Tasks.kt
@@ -3,6 +3,21 @@ package sp.kx.gradlex
 import org.gradle.api.Task
 import org.gradle.api.tasks.TaskContainer
 
+/**
+ * Locates a [Task] by name segments in camel case, failing if there is no such object.
+ *
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val task = project.tasks.get("foo", "bar") {
+ *     ...
+ * }
+ * assertEquals("fooBar", task.name)
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.0
+ * @see TaskContainer.getByName
+ */
 inline fun <reified T : Task> TaskContainer.get(
     name0: String,
     name1: String,
@@ -14,6 +29,22 @@ inline fun <reified T : Task> TaskContainer.get(
     return task
 }
 
+/**
+ * Creates a [Task] with the given name segments in camel case,
+ * configures it with the given [block], and adds it to [this].
+ *
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val task = project.tasks.create("foo", "bar") {
+ *     ...
+ * }
+ * assertEquals("fooBar", task.name)
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.0
+ * @see TaskContainer.create
+ */
 fun TaskContainer.create(
     name0: String,
     name1: String,
@@ -24,6 +55,22 @@ fun TaskContainer.create(
     return create(name, block)
 }
 
+/**
+ * Creates a [Task] with the given name segments in camel case and [T] type,
+ * configures it with the given [block], and adds it to [this].
+ *
+ * Usage:
+ * ```
+ * val project: Project = ...
+ * val task = project.tasks.add<DefaultTask>("foo", "bar") {
+ *     ...
+ * }
+ * assertEquals("fooBar", task.name)
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.1.0
+ * @see TaskContainer.create
+ */
 inline fun <reified T : Task> TaskContainer.add(
     name0: String,
     name1: String,
